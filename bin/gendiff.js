352fcd6b99573068d763418acafa7266763f5aeb
Victor Kim <kim.victor.aleksandrovich@gmail.com>
2021-08-22 16:39:40 +0600

    feat: add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import commander from 'commander';
 const program = new commander.Command();
 
 program
-  .version('0.0.1', '-v, --version', 'output the current version')
+  .version('0.0.1', '-v, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse();
 
 export default program;
